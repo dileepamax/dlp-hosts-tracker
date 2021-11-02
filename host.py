beb21b13895ba97a912b55ab92f79b210506dfaf
dileepa <dileepamax@gmail.com>
2021-11-02 15:09:26 +0530

    changes to host details

--- host.py
+++ host.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
 class Host:
-    def __init__(self, ip_address, ping, user_name, time_alive):
-        self.ip_address = ip_address
+    def __init__(self, ip_address, ping, username, timealive):
+		self.ip_address = ip_address
         self.ping = ping
-        self.user_name = user_name
-        self.time_alive = 0
+        self.username = username
+        self.timealive = 0
+		self.type = type
+		
 
     def sod(self):
         pass
+       
+	def test(self):
+		pass
+
